5d58180b3a2e5558bba02306ed073c3c8f635825
candidosales <candidosg@gmail.com>
2022-08-22 02:07:44 -0400

    remove comments

--- vite.config.ts
+++ vite.config.ts
@@ -13,9 +13,6 @@ export default defineConfig({
     brotliSize: false,
     rollupOptions: {
       inlineDynamicImports: true,
-      // output: {
-      //   manualChunks: () => "everything.js",
-      // },
     },
   },
 });
